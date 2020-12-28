220ffe31dec7127df159c78f0805addd3394d332
Daniel Eden <dan.eden@me.com>
2020-12-28 19:02:52 +0000

    Try to fix runtime error

--- ebooks.py
+++ ebooks.py
@@ -65,7 +65,7 @@ if __name__=="__main__":
     if guess == 0:
         if STATIC_TEST==True:
             file = TEST_SOURCE
-            print ">>> Generating from {0}".format(file)
+            print(">>> Generating from {0}".format(file))
             string_list = open(file).readlines()
             for item in string_list:
                 source_tweets = item.split(",")
@@ -78,9 +78,9 @@ if __name__=="__main__":
                 for x in range(17)[1:]:
                     source_tweets_iter, max_id = grab_tweets(api,max_id)
                     source_tweets += source_tweets_iter
-                print "{0} tweets found in {1}".format(len(source_tweets), handle)
+                print("{0} tweets found in {1}".format(len(source_tweets), handle))
                 if len(source_tweets) == 0:
-                    print "Error fetching tweets from Twitter. Aborting."
+                    print("Error fetching tweets from Twitter. Aborting.")
                     sys.exit()
         mine = markov.MarkovChainer(order)
         for tweet in source_tweets:
@@ -95,15 +95,15 @@ if __name__=="__main__":
 
         #randomly drop the last word, as Horse_ebooks appears to do.
         if random.randint(0,4) == 0 and re.search(r'(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$', ebook_tweet) != None:
-           print "Losing last word randomly"
+           print("Losing last word randomly")
            ebook_tweet = re.sub(r'\s\w+.$','',ebook_tweet)
-           print ebook_tweet
+           print(ebook_tweet)
 
         #if a tweet is very short, this will randomly add a second sentence to it.
         if ebook_tweet != None and len(ebook_tweet) < 20:
             rando = random.randint(0,10)
             if rando == 0 or rando == 7:
-                print "Short tweet. Adding another sentence randomly"
+                print("Short tweet. Adding another sentence randomly")
                 newer_tweet = mine.generate_sentence()
                 if newer_tweet != None:
                     ebook_tweet += " " + mine.generate_sentence()
@@ -111,7 +111,7 @@ if __name__=="__main__":
                     ebook_tweet = ebook_tweet
             elif rando == 1:
                 #say something crazy/prophetic in all caps
-                print "ALL THE THINGS"
+                print("ALL THE THINGS")
                 ebook_tweet = ebook_tweet.upper()
 
         #throw out tweets that match anything from the source account.
@@ -120,18 +120,18 @@ if __name__=="__main__":
                 if ebook_tweet[:-1] not in tweet:
                     continue
                 else:
-                    print "TOO SIMILAR: " + ebook_tweet
+                    print("TOO SIMILAR: " + ebook_tweet)
                     sys.exit()
 
             if DEBUG == False:
                 status = api.PostUpdate(ebook_tweet)
-                print status.text.encode('utf-8')
+                print(status.text.encode('utf-8'))
             else:
-                print ebook_tweet
+                print(ebook_tweet)
 
         elif ebook_tweet == None:
-            print "Tweet is empty, sorry."
+            print("Tweet is empty, sorry.")
         else:
-            print "TOO LONG: " + ebook_tweet
+            print("TOO LONG: " + ebook_tweet)
     else:
-        print str(guess) + " No, sorry, not this time." #message if the random number fails.
+        print(str(guess) + " No, sorry, not this time.") #message if the random number fails.
